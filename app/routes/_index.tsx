7c55305d28eb5843008f4d990d5f2190e47747b9
marlonmp <marlonmp214@protonmail.com>
2024-04-17 01:21:09 -0500

    base project addd

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1 className="text-neutral-50 font-bold text-4xl">Welcome to Remix Todo list!!</h1>
+    </div>
+  );
+}
