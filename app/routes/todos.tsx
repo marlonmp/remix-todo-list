a7b4446dbec523eed469c2db260d3ecbed5e2682
marlonmp <marlonmp214@protonmail.com>
2024-04-17 11:03:17 -0500

    todo toggle added and created added

--- app/routes/todos.tsx
+++ app/routes/todos.tsx
@@ -1,6 +1,8 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
+import moment from "moment";
+
 type TodoItem = {
     id: string,
     title: string,
@@ -26,14 +28,32 @@ function NoItem() {
     );
 }
 
-function Item({ id, title, description, is_done, date } : TodoItem) {
+function Toggle({ is_active }: { is_active: boolean }) {
+    const period = <div className="w-3 h-3 rounded-full bg-blue-500"></div>;
+
+    return (
+        <div className="w-6 h-6 rounded-full border-2 border-blue-500 hover:cursor-pointer">
+            <div className="w-full h-full flex justify-center items-center">
+                {is_active ? period : undefined}
+            </div>
+        </div>
+    );
+}
+
+function Item({ id, title, description, is_done, created } : TodoItem) {
     const activeClass = !is_done ? '' : 'line-through italic'
 
     return (
-        <div className="w-full rounded-lg p-8 bg-neutral-700">
-            <div className="w-full flex flex-col">
-                <div className={`text-2xl text-white ${activeClass}`}>{title}</div>
-                <div className="text-lg text-neutral-500">{description}</div>
+        <div className="w-full rounded-lg px-8 py-6 bg-neutral-700">
+            <div className="w-full flex flex-row space-x-4">
+                <div className="pt-1">
+                    <Toggle is_active={is_done} />
+                </div>
+                <div className="flex-grow flex flex-col">
+                    <div className={`text-lg text-white ${activeClass}`}>{title}</div>
+                    <div className="text-md text-neutral-400">{description}</div>
+                    <div className="text-sm text-neutral-500">Created at: {moment(created).format('LLL')}</div>
+                </div>
             </div>
         </div>
     );
@@ -42,11 +62,16 @@ function Item({ id, title, description, is_done, date } : TodoItem) {
 export default function TodoList() {
     const { todos } = useLoaderData() as any;
 
-    console.log(todos);
-
     const items = !todos?.length ? <NoItem /> : todos.map((item: TodoItem) => <Item key={item.id} {...item} />);
 
     return (
-        <div className="w-full flex flex-col items-center space-y-4">{items}</div>
+        <div className="w-full flex flex-col space-y-6">
+            <div className="w-full flex flex-row justify-between items-center">
+                <div className="font-bold text-white">Todo list</div>
+                <div className="px-4 py-2 font-bold rounded-md text-white bg-green-600 hover:cursor-pointer hover:bg-green-700">Add todo</div>
+            </div>
+
+            <div className="w-full flex flex-col items-center space-y-4">{items}</div>
+        </div>
     );
 }
